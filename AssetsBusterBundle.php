f39e76fa247525f3a24fbe69a1f2da3422544f2a
Andrey Holodnyak <andrey.holodnyak@gmail.com>
2016-12-23 03:49:37 +0300

    Bundle structure

---
+++ AssetsBusterBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AVHolodnyak\AssetsBusterBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * This bundle provides support for cache busting using buster files.
+ * It uses custom version strategies feature that was introduced in Symfony 3.1.
+ *
+ * @author Andrey Holodnyak <andrey.holodnyak@gmail.com>
+ * @package AssetsBusterBundle
+ */
+class AssetsBusterBundle extends Bundle
+{
+
+}
